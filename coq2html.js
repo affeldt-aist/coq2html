112868c584ccf9372d72eceef5fc208e2f2a923b
Yoshihiro Imai <y.imai@aist.go.jp>
2023-11-24 13:01:28 +0900

    feature: introduce markdown mode for ssrdocs

---
+++ coq2html.js
@@ -0,0 +1,15 @@
+function renderMarkdowns()
+{
+    const md = markdownit({html:true})
+          .use(texmath, { engine: katex,
+                          delimiters: 'dollars'} );
+    const elements = document.querySelectorAll('.markdown');
+    for (let elem of elements) {
+	elem.innerHTML = md.render(elem.textContent);
+    }
+}
+
+function init()
+{
+    renderMarkdowns();
+}
