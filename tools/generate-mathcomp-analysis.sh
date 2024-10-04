ebb2fd2ed3187f0273f31f9e9c284d2f0861c295
Yoshihiro Imai <y.imai@aist.go.jp>
2024-10-04 13:53:48 +0900

    CI: :construction_worker: hierarchy graph

--- tools/generate-mathcomp-analysis.sh
+++ tools/generate-mathcomp-analysis.sh
@@ -4,8 +4,6 @@ MATHCOMP_ANALYSIS=./analysis
 DIR=$(pwd `dirname .`)
 OUTDIR=$DIR/html
 
-make clean && make
-
 rm -rf $OUTDIR
 mkdir $OUTDIR
 
@@ -15,4 +13,11 @@ ls -l
 
 FILES=$(find classical/ theories/ -name "*.v" -or -name "*.glob")
 
-$DIR/coq2html -title "Mathcomp Analysis" -d $OUTDIR -base mathcomp -Q theories analysis -coqlib https://coq.inria.fr/doc/V8.18.0/stdlib/ -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.ssreflect -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.algebra $FILES
+$DIR/tools/generate-hierarchy-graph.sh
+
+$DIR/coq2html -title "Mathcomp Analysis" -d $OUTDIR -base mathcomp \
+  -Q theories analysis -coqlib https://coq.inria.fr/doc/V8.18.0/stdlib/ \
+  -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.ssreflect \
+  -external https://math-comp.github.io/htmldoc_2_1_0/ mathcomp.algebra \
+  -hierarchy-graph "hierarchy-graph.dot" \
+  $FILES
